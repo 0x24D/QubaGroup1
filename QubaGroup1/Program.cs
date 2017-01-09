5b75abdd87ed8f5db576cd6a769cf180207c387e
0x24d <daniel.longfield@tuta.io>
2017-01-09 10:43:31 +0000

    Implemented NUnit into ping test

--- QubaGroup1/Program.cs
+++ QubaGroup1/Program.cs
@@ -11,33 +11,9 @@ namespace QubaGroup1
     class Program
 
     {
-        static string url = "http://shugroupproject1.quba.co.uk/";
         static void Main(string[] args)
         {
-            Ping(url);
         }
-        static public bool Ping(string url)
-        {
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-                request.Timeout = 3000;
-                request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
-                request.Method = "HEAD";
-
-                using (var response = request.GetResponse()) //The website is live
-                {
-                    Console.WriteLine("Website Online.");
-                    Console.ReadLine();
-                    return true;
-                }
-
-            }
-            catch
-            {
-                return false;
-            }
-
-        }
+        
     }
 }
---
+++ QubaGroup1/WebsiteUpTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace QubaGroup1
+{
+    public class WebsiteUpTest
+    {
+        [TestCase("http://shugroupproject1.quba.co.uk/")]
+        public void TestCase(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.Timeout = 3000;
+                request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
+                request.Method = "HEAD";
+
+                using (var response = request.GetResponse())
+                {
+
+                }
+
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+        }
+    }
+}
