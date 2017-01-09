7d6f52d37b78319bb282f5b458302c8350264ade
Aracnoss <michael.walls070@gmail.com>
2017-01-09 12:05:37 +0000

    Got initial setup of CompareFiles done.

--- QubaGroup1/CompareFiles.cs
+++ QubaGroup1/CompareFiles.cs
@@ -8,5 +8,20 @@ namespace QubaGroup1
 {
     class CompareFiles
     {
+        //check names of "to be deployed" files
+        private string[] getFileNames(string URL)
+        {
+            string[] FileNames = new string[5];
+            return FileNames;
+        }
+
+        //find and get files with same name from "live" server
+        //compare to files from "to be deployed" area
+        public bool compareFiles(string fileNames, string URL)
+        {
+            getFileNames(URL);
+            return true;
+        }
+
     }
 }
