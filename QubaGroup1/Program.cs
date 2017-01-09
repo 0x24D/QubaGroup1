019485132eaf27e2ed6773de9ceec03d1030cf90
M1K <mikepress88@gmail.com>
2017-01-09 10:15:50 +0000

    Added Ping capability

--- QubaGroup1/Program.cs
+++ QubaGroup1/Program.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace QubaGroup1
 {
     class Program
+
     {
+        static string url = "http://shugroupproject1.quba.co.uk/";
         static void Main(string[] args)
         {
-            hkhkjhk
+            Ping(url);
+        }
+        static public bool Ping(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.Timeout = 3000;
+                request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
+                request.Method = "HEAD";
+
+                using (var response = request.GetResponse())
+                {
+                    Console.WriteLine("print");
+                    Console.ReadLine();
+                    return true;
+                }
+
+            }
+            catch
+            {
+                return false;
+            }
+
         }
     }
 }
