c542fe7a04fe6d19b696bd6aefe136c6fef15b2d
Aracnoss <michael.walls070@gmail.com>
2017-01-09 13:22:06 +0000

    Started heavily working on the comparing of files, Now needs Lib LibGit2Sharp.

--- QubaGroup1/CompareFiles.cs
+++ QubaGroup1/CompareFiles.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibGit2Sharp;
+using NUnit.Framework;
+using NUnit;
 
 namespace QubaGroup1
 {
     class CompareFiles
     {
-        //check names of "to be deployed" files
-        private string[] getFileNames(string URL)
+        //Creates a "file" which can tell us the name and it's status in the latest commit.
+        public class file
         {
-            string[] FileNames = new string[5];
-            return FileNames;
+            public string Name {get; set;}
+            public ChangeKind State {get; set;}
+        }
+        List<file> files = new List<file>();
+
+        //check names of "to be deployed" files
+        //Probably using files that have been edited/added/deleted recently ie. "today".        
+        //[TestCase("")]
+        private void getFileNames(string Repository)
+        {
+            using (Repository repo = new Repository(Repository))
+            {
+                Tree commitTree = repo.Head.Tip.Tree; // Main Tree
+                Tree parentCommitTree = repo.Head.Tip.Parents.Single().Tree; // Secondary Tree
+
+                var patch = repo.Diff.Compare<Patch>(parentCommitTree, commitTree); // Difference
+
+                foreach (var ptc in patch)
+                {
+                    file file = new file();
+                    file.Name = ptc.Path;
+                    file.State = ptc.Status;
+                    files.Add(file);
+                }
+            }
         }
 
         //find and get files with same name from "live" server
