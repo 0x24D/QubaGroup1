987a1ac7d589e979b3ed4de15702a3e61bf46afd
jackwoodhouse <jack.woodhouse.97@gmail.com>
2017-01-10 13:12:41 +0000

    added ping to html link

--- QubaGroup1/PingTest.cs
+++ QubaGroup1/PingTest.cs
@@ -12,24 +12,53 @@ namespace QubaGroup1
     public class PingTest
     {
         [TestCase("http://shugroupproject1.quba.co.uk/")]
-        public void TestCase(string url)
+        public void TestCase(string url1)
         {
             try
             {
-                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url1);
                 request.Timeout = 3000;
                 request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
                 request.Method = "HEAD";
 
                 using (var response = request.GetResponse())
                 {
-
+                    Console.WriteLine("working --  Shugroupproject1.quba.co.uk is online");
+                    Console.ReadLine();
                 }
 
             }
             catch
             {
                 Assert.Fail();
+            }
+
+        }
+    }
+
+    public class CheckLinks
+    {
+        [TestCase("http://shugroupproject1.quba.co.uk/product")]
+        public void TestCase(string url2)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url2);
+                request.Timeout = 3000;
+                request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
+                request.Method = "HEAD";
+
+                using (var response = request.GetResponse())
+                {
+                    Console.WriteLine("working --  Shugroupproject1.quba.co.uk/products is online");
+                    Console.ReadLine();
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("failed --  Shugroupproject1.quba.co.uk/product is not online");
+                Console.ReadLine();
             }
 
         }
--- QubaGroup1/Program.cs
+++ QubaGroup1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,15 +12,21 @@ namespace QubaGroup1
 {
     class Program
     {
-        static string URL = "http://shugroupproject1.quba.co.uk/";
+        static string URL1 = "http://shugroupproject1.quba.co.uk"; // --  needs to changing so it not hard coded
+        static string URL2 = "http://shugroupproject1.quba.co.uk/product"; // --  needs to changing so it not hard coded
+
         static string Repository = "https://quba.svn.beanstalkapp.com/shu-group-project-1/";
         static string filePath;
         static void Main(string[] args)
         {
             PingTest ping = new PingTest();
             CompareFiles CmpFiles = new CompareFiles();
-            ping.TestCase(URL);
+            CheckLinks check = new CheckLinks(); // added this into PingTest however i am unsure of the potenial uses for it 
+
+            ping.TestCase(URL1);
+            check.TestCase(URL2);
             CmpFiles.getFileDetails(Repository, filePath);
+           
         }
         
     }
