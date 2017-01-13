ca3fbbb58f3e99538d32f3863482fa27ac533786
Aracnoss <michael.walls070@gmail.com>
2017-01-13 11:39:06 +0000

    Preparing local demos for compareTheFiles

--- QubaGroup1/Program.cs
+++ QubaGroup1/Program.cs
@@ -28,16 +28,16 @@ namespace QubaGroup1
             CompareFiles cmpFiles = new CompareFiles();
             LinkTest check = new LinkTest(); // added this into PingTest however i am unsure of the potenial uses for it 
 
-            p.Ping(URL1);
-            check.TestCase(URL2);
+//            p.Ping(URL1);
+//            check.TestCase(URL2);
 //            cmpFiles.getFileDetails(Repository, filePath);
-//            cmpFiles.compareTheFiles(URL1, Repository, filePath);
-            string sum1 = cmpFiles.CalculateMd5Hash("C:\\Users\\b5021991\\Desktop\\Test.txt");
-            string sum2 = cmpFiles.CalculateMd5Hash("C:\\Users\\b5021991\\Desktop\\Test2.txt");
-            if (sum1.Equals(sum2))
-                Console.WriteLine("MD5sums match");
-            else
-                Console.WriteLine("MD5sums do not match");
+            cmpFiles.compareTheFiles(URL1, Repository, filePath);
+//            string sum1 = cmpFiles.CalculateMd5Hash("C:\\Users\\b5021991\\Desktop\\Test.txt");
+//            string sum2 = cmpFiles.CalculateMd5Hash("C:\\Users\\b5021991\\Desktop\\Test2.txt");
+//            if (sum1.Equals(sum2))
+//                Console.WriteLine("MD5sums match");
+//            else
+//                Console.WriteLine("MD5sums do not match");
             Console.ReadLine();
         }
         
